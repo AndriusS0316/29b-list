c3ca85aeb790981a758bb711c1630df4f10e4283
Andrius <andriuss0316@gmail.com>
2021-07-13 09:56:59 +0300

    20210713

---
+++ js/components/Dictionary.js
@@ -0,0 +1,11 @@
+class Dictionary {
+    constructor(selector, pavadinimas, list) {
+        // console.log('zodynas pasileido...');
+        // console.log(name);
+        this.selector = selector;
+        this.name = pavadinimas;
+        this.dictionaryList = list;
+    }
+}
+
+export { Dictionary }
